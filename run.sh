b9f90947709b10420c293acb3a29801e6d6aa216
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2024-02-13 14:02:19 +0100

    up

--- run.sh
+++ run.sh
@@ -27,3 +27,8 @@ if [[ $1 == "detectbursts" ]]
 then
     python -O BurstDetection.py $2 $SLURM_ARRAY_TASK_ID "task" 0
 fi
+
+if [[ $1 == "detectbursts3D" ]]
+then
+    python -O BurstDetection3D.py $2 $SLURM_ARRAY_TASK_ID "task" 0
+fi
